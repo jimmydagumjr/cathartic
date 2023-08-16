55080d38975749f26565f84bb967678cb75aa79d
jimmy <jimmydagumjr@gmail.com>
2023-08-16 09:19:17 -0700

    styling + tiny fixes

--- navbar.js
+++ navbar.js
@@ -1,4 +1,4 @@
-window.addEventListener("scroll", function() {
+window.addEventListener("scroll", () => {
     /*
     let header = document.querySelector(".mainmenu");
     header.classList.toggle("sticky", window.scrollY > 0);
