1c989745cd35031833222d868f1733077a705967
jimmy <jimmydagumjr@gmail.com>
2023-08-16 08:38:08 -0700

    dynamic navbar logo change

--- navbar.js
+++ navbar.js
@@ -3,4 +3,8 @@ window.addEventListener("scroll", function() {
     let header = document.querySelector(".mainmenu");
     header.classList.toggle("sticky", window.scrollY > 0);
     */
+    let clogo = document.querySelector(".cathartic-logo");
+    let linelogo = document.querySelector(".line-logo");
+    clogo.classList.toggle("-flip", window.scrollY > 0);
+    linelogo.classList.toggle("-flip", window.scrollY > 0);
 })
