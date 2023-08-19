d9bc0520c426d4f4c4121619d1ef3a69bd02bff9
jimmy <jimmydagumjr@gmail.com>
2023-08-19 03:41:51 -0700

    updated cover art

--- scripts/data/songs.js
+++ scripts/data/songs.js
@@ -16,21 +16,21 @@ const songs = [
     {
       title: "ibihywnm",
       artist: "st jimmy",
-      img_src: "1.jpg",
+      img_src: "IMG-8262.jpg",
       src: "https://files.catbox.moe/6li9j3.mp3",
       url: "https://open.spotify.com/track/6OhIxbWku0wXCS11k6J333?si=8daf8f6fe72c4910"
     },
     {
       title: "trauma bonds",
       artist: "st jimmy",
-      img_src: "1.jpg",
+      img_src: "IMG_6240.jpg",
       src: "https://files.catbox.moe/clb9ud.mp3",
       url: "https://open.spotify.com/track/21oTqs4tULaZtxOaecCTF0?si=01efbf381620469a"
     },
     {
       title: "i'm so tired (cover)",
       artist: "st jimmy",
-      img_src: "1.jpg",
+      img_src: "IMG-2687.jpg",
       src: "https://files.catbox.moe/cm4xm9.mp3",
       url: null
     },
@@ -44,7 +44,7 @@ const songs = [
     {
       title: "old tattoos",
       artist: "st jimmy",
-      img_src: "1.jpg",
+      img_src: "IMG-5213.jpg",
       src: "https://files.catbox.moe/u6796o.mp3",
       url: "https://open.spotify.com/track/3EhIdQSKO7AwGK9of76U0Z"
     },
