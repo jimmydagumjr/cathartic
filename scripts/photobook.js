8a59c11cf86c9118b16b2d951e117aad67b3f5ad
jimmy <jimmydagumjr@gmail.com>
2023-08-19 03:18:21 -0700

    photobook script + data reorganization

---
+++ scripts/photobook.js
@@ -0,0 +1,38 @@
+const photobookContainer = document.querySelector("#photo-container");
+
+updatePhotobook(photos);
+
+function updatePhotobook(photos) {
+
+    //remove any existing element in photobook container
+    photobookContainer.innerHTML = "";
+
+    //use for each on photos array
+    photos.forEach(photo => {
+        //extract data from photo
+        const {location, src, time} = photo;
+        const div = document.createElement("div");
+
+        //format time from photo
+        formattedTime = time.toLocaleDateString(undefined, {
+            year: "numeric",
+            month: "long",
+            day: "numeric"
+        })
+
+        div.classList.add("card");
+        div.innerHTML = `
+        <div class="card">
+          <div class="card__img-container">
+            <img class="card__img" src="${src}">
+          </div>
+          <div class="card__details">
+            <h2 class="card__location">${location}</h2>
+            <p class="card__time">${formattedTime}</p>
+          </div>
+        </div>
+        `;
+
+        photobookContainer.appendChild(div);
+    });
+}
